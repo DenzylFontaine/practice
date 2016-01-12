7667e4345d9205a1bc37779639e7b005da89d046
DenzylFontaine <ddenzfong@gmail.com>
2016-01-11 21:59:13 -0700

    practicing deployments

---
+++ pracindex.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Alabama vs CLemson;
+		Bears and Tigers meet for the first time this season to have a battle we will all remember!</title>
+	</head>
+	<body></body>
+	<p>
+	<?php
+	echo <<<_end
+Desawn watson and the clemson offense just didn't have enough fire power to compete with alabama's all around game.
+Over some time it was evident that the game was at hand.
+- Denz
+_end;
+?>
+	</p>
+</html>
+
+
+<?php
+function longdate($timestamp)
+{
+	return date("l F jS Y", $timestamp);
+}
+?>
