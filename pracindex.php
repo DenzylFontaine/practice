24bd06bb4e7bc8ea80bd7582fb6031e1078124f7
DenzylFontaine <ddenzfong@gmail.com>
2016-01-11 21:45:07 -0700

    practicing deployments

---
+++ pracindex.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Alabama vs CLemson;
+		Bears and Tigers, oh my!!</title>
+	</head>
+	<body></body>
+	<p>I predict clemson will pull of a last second victory</p>
+</html>
+
+
+<?php
+function longdate($timestamp)
+{
+	return date("l F jS Y", $timestamp);
+}
+?>
